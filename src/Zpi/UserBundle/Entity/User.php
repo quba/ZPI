c38475458b3843ffc8bdd71acbcf8305aaf81c1d
Kuba Bomba <quba@quba.info>
2011-10-04 14:50:19 +0200

    Nadpisany formularz rejestracji - teraz można go dowolnie modelować

--- src/Zpi/UserBundle/Entity/User.php
+++ src/Zpi/UserBundle/Entity/User.php
@@ -16,11 +16,94 @@ class User extends BaseUser
     /**
      * @var integer $id
      *
-     * @ORM\Column(name="id", type="integer")
+     * @ORM\Column(name="id", type="integer", nullable=true)
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-    protected $id;
+    protected $id;    
+
+    /**
+     * @var string $title
+     *
+     * @ORM\Column(name="title", type="string", length=25, nullable=true)
+     */
+    protected $title;
+
+    /**
+     * @var string $name
+     *
+     * @ORM\Column(name="name", type="string", length=50, nullable=true)
+     */
+    protected $name;
+    
+    /**
+     * @var string $surname
+     *
+     * @ORM\Column(name="surname", type="string", length=50, nullable=true)
+     */
+    protected $surname;
+    
+    /**
+     * @var string $institution
+     *
+     * @ORM\Column(name="institution", type="string", length=50, nullable=true)
+     */
+    protected $institution;
+    
+    /**
+     * @var string $address
+     *
+     * @ORM\Column(name="address", type="string", length=50, length=50, nullable=true)
+     */
+    protected $address;
+    
+    /**
+     * @var string $city
+     *
+     * @ORM\Column(name="city", type="string", length=50, nullable=true)
+     */
+    protected $city;
+    
+    /**
+     * @var string $postalcode
+     *
+     * @ORM\Column(name="postalcode", type="string", length=50, nullable=true)
+     */
+    protected $postalcode;
+    
+    /**
+     * @var string $country
+     *
+     * @ORM\Column(name="country", type="string", length=50, nullable=true)
+     */
+    protected $county;
+    
+    /**
+     * @var string $phone
+     *
+     * @ORM\Column(name="phone", type="string", length=50, length=50, nullable=true)
+     */
+    protected $phone;
+    
+    /**
+     * type = private participation (0) || invoice for the institution (1)
+     * 
+     * @var integer $type
+     *
+     * @ORM\Column(name="type", type="integer", nullable=true)
+     */
+    protected $type;
+    
+    /**
+     * @var string $nipvat
+     *
+     * @ORM\Column(name="nipvat", type="string", length=50, length=50, nullable=true)
+     */
+    protected $nipvat;
+    
+
+    
+
     
     public function __construct()
     {
@@ -43,5 +126,225 @@ class User extends BaseUser
     {
         parent::setEmail($email);
         $this->setUsername($email); 
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string 
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set surname
+     *
+     * @param string $surname
+     */
+    public function setSurname($surname)
+    {
+        $this->surname = $surname;
+    }
+
+    /**
+     * Get surname
+     *
+     * @return string 
+     */
+    public function getSurname()
+    {
+        return $this->surname;
+    }
+
+    /**
+     * Set institution
+     *
+     * @param string $institution
+     */
+    public function setInstitution($institution)
+    {
+        $this->institution = $institution;
+    }
+
+    /**
+     * Get institution
+     *
+     * @return string 
+     */
+    public function getInstitution()
+    {
+        return $this->institution;
+    }
+
+    /**
+     * Set address
+     *
+     * @param string $address
+     */
+    public function setAddress($address)
+    {
+        $this->address = $address;
+    }
+
+    /**
+     * Get address
+     *
+     * @return string 
+     */
+    public function getAddress()
+    {
+        return $this->address;
+    }
+
+    /**
+     * Set city
+     *
+     * @param string $city
+     */
+    public function setCity($city)
+    {
+        $this->city = $city;
+    }
+
+    /**
+     * Get city
+     *
+     * @return string 
+     */
+    public function getCity()
+    {
+        return $this->city;
+    }
+
+    /**
+     * Set postalcode
+     *
+     * @param string $postalcode
+     */
+    public function setPostalcode($postalcode)
+    {
+        $this->postalcode = $postalcode;
+    }
+
+    /**
+     * Get postalcode
+     *
+     * @return string 
+     */
+    public function getPostalcode()
+    {
+        return $this->postalcode;
+    }
+
+    /**
+     * Set county
+     *
+     * @param string $county
+     */
+    public function setCounty($county)
+    {
+        $this->county = $county;
+    }
+
+    /**
+     * Get county
+     *
+     * @return string 
+     */
+    public function getCounty()
+    {
+        return $this->county;
+    }
+
+    /**
+     * Set phone
+     *
+     * @param string $phone
+     */
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    /**
+     * Get phone
+     *
+     * @return string 
+     */
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    /**
+     * Set type
+     *
+     * @param integer $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * Get type
+     *
+     * @return integer 
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * Set nipvat
+     *
+     * @param string $nipvat
+     */
+    public function setNipvat($nipvat)
+    {
+        $this->nipvat = $nipvat;
+    }
+
+    /**
+     * Get nipvat
+     *
+     * @return string 
+     */
+    public function getNipvat()
+    {
+        return $this->nipvat;
     }
 }
