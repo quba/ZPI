a04ee9299cc1f8c919b655d0f73341e681c3d41c
Kuba Bomba <quba@quba.info>
2011-10-12 21:15:24 +0200

    Wstęp do zarządzania użytkownikami

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Zpi\UserBundle\ZpiUserBundle(),
             new Zpi\PageBundle\ZpiPageBundle(),
+            new Zpi\UserManagementBundle\ZpiUserManagementBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
