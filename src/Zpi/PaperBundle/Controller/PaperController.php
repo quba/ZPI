22e5b582c8857a4bc31398d655c6da3aac9dda49
Kuba Bomba <quba@quba.info>
2011-11-04 00:20:33 +0100

    Papieru użytkownika - teraz już wydajnie, ale wg mnie można to zrobić bardziej elegancko. Opis problemu w komentarzach.

--- src/Zpi/PaperBundle/Controller/PaperController.php
+++ src/Zpi/PaperBundle/Controller/PaperController.php
@@ -44,9 +44,27 @@ class PaperController extends Controller
     
     public function showAction()
     {
-	//$user = $this->get('security.context')->getToken()->getUser();
-	//$papers = $user->getAuthorPapers();	
-	return $this->render('ZpiPaperBundle:Paper:show.html.twig');
+	$user = $this->get('security.context')->getToken()->getUser();
+	/*$papers = $user->getAuthorPapers(); 
+         * funkcja ta niestety pobiera tylko dane z tabeli users_papers i jest to prawidłowe działanie, bo mamy tam one to 
+         * many i nie przeskoczymy przez tę tabelę łatwo (żeby pobrać title i inne z papers). Próbowałem tworzyć tzw. proxy
+         * metodę w UserPaper (która zwracała getPaper()->getTitle()), jednak to rozwiązanie nie jest wg mnie zbyt wydajne.
+         * Owszem działa, ale np. pobranie wszystkich nazw paperów danego usera (standardowe getAuthorPapers()) skutkuje
+         * najpierw pobraniem wszystkich rekordów danego usera o typie 0 z tabeli users_papers (OK), jednak potem gdy chcemy
+         * odwołać się do tytułu papera (nie ma go w users_papers), korzysta ze stworzonej przeze mnie proxy metody tej klasy
+         * o nazwie getTitle(). Wszystko jest spoko, jednak daje nam to dla każdego elementu usera z users_papers dodatkowe
+         * zapytanie, które pobiera ten tytuł. Rozwiązanie schludne i wygodne, jednak przydatne tylko przy pobieraniu jednego
+         * rekordu (choć i tutaj bym się zastanawiał, bo mamy 2 zapytania, a można to zrobić jednym inner joinem).
+         * Dyskusja na ten temat oraz coś o proxy metodzie dla tej asocjacji: 
+         * http://stackoverflow.com/questions/3542243/doctrine2-best-way-to-handle-many-to-many-with-extra-columns-in-reference-table
+         */
+        
+        $em = $this->getDoctrine()->getEntityManager();
+        $papers = $em->createQuery('SELECT p, up FROM ZpiPaperBundle:UserPaper up INNER JOIN up.paper p where up.user = :uid')
+                ->setParameter('uid', $user->getId())
+                ->execute();
+        
+	return $this->render('ZpiPaperBundle:Paper:show.html.twig', array('papers' => $papers));
     }
     
     public function detailsAction($id)
@@ -54,13 +72,15 @@ class PaperController extends Controller
 	$user = $this->get('security.context')->getToken()->getUser();
 	//$paper = $user->getPapers()->get($id);
         
-        $query = $this->getDoctrine()->getEntityManager()->createQuery(
-            'SELECT p FROM ZpiPaperBundle:UserPaper p 
-                WHERE p.paper = :id AND p.user = :uid'
+        $paper = $this->getDoctrine()->getEntityManager()->createQuery(
+            'SELECT p, up FROM ZpiPaperBundle:UserPaper up INNER JOIN up.paper p
+                WHERE up.paper = :id AND up.user = :uid'
             )->setParameter('id', $id)
-            ->setParameter('uid', $user->getId());
-	
-        $paper = $query->getSingleResult();
+             ->setParameter('uid', $user->getId()) // nie chcemy, żeby inni userzy widzieli narze pejpery
+             ->getSingleResult();
+        /* nie mam już nerwów, żeby z jakichś querybuilderów korzystać. Takie zapytanie jest najbardziej optymalne,
+           a jak ktoś znajdzie, jak je wywołać jakoś bezpośrednio pobierając np. $user->getPaper($id) to ma ode mnie
+           browara i dodatniego plusa. */
 	
 	if(!$paper)
 	{
